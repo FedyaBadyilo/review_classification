4c8268fe13a4576527711c9f8b92f43d83f9d902
Badylo Fedor <fedya20044@mail.ru>
2024-09-19 19:33:41 +0300

    some error

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ async def predict(request: Request, review_text: str = Form(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, port=8000)
 
 
